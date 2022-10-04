f79a72bacfe7de8c0b04c1750254bae87bd5e21d
eblyncho <eblyn52@naver.com>
2022-10-04 16:38:14 +0900

    I/O 예제 1

---
+++ ch13/FileReaderEx.java
@@ -0,0 +1,23 @@
+package ch13;
+
+import java.io.FileReader;
+
+// java 패키지명.클래스명
+// java ch13.FileReaderEx
+
+public class FileReaderEx {
+
+	public static void main(String[] args) {
+		try {
+			FileReader fr = new FileReader("ch13/aaa.txt");
+			int i;	// 파일의 마지막에 있는 값이 -1
+			while ((i=fr.read())!=-1) {
+				System.out.print((char)i);
+			}
+			fr.close();
+			System.out.println("\nEnd~~");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ ch13/FileWriterEx.java
@@ -0,0 +1,20 @@
+package ch13;
+
+import java.io.FileWriter;
+
+public class FileWriterEx {
+
+	public static void main(String[] args) {
+		String str = "Dream as if you'll live forever,\n"
+				+ "Live as if you'll die today - ¡¶¿”Ω∫µÚ";
+		try {
+			FileWriter fw = new FileWriter("ch13/bbb.txt");
+			fw.write(str);
+			fw.flush();
+			fw.close();
+			System.out.println("End~~");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
