285cb09e760e6e4169f314929c2278f415299aeb
eblyncho <eblyn52@naver.com>
2022-10-11 17:00:40 +0900

    Interface

---
+++ ch13/InterEx1.java
@@ -0,0 +1,30 @@
+package ch13;
+
+interface A {
+	int i = 10;  // 자동 상수; static final
+	void prn();  // 메소드는 선언부만, 구현부는 없음
+}
+
+interface Calcu {
+	void plus(int i, int j);
+}
+
+class Function extends Object implements Calcu {
+	@Override
+	public void plus(int i, int j) {
+	}
+}
+
+class Graphic implements Calcu {
+	@Override
+	public void plus(int i, int j) {
+	}
+}
+
+public class InterEx1 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
