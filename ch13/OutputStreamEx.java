283f1bb21209c89eabe2e8a66b7bac721a69eaa8
eblyncho <eblyn52@naver.com>
2022-10-17 12:38:16 +0900

    Output Stream

--- ch13/OutputStreamEx.java
+++ ch13/OutputStreamEx.java
@@ -1,24 +1,23 @@
 package ch13;
-
 import java.io.IOException;
 import java.io.OutputStream;
-
 public class OutputStreamEx {
-	
-	public static void main(String[] args) /*throws Exception*/ {
-		OutputStream out = System.out;
-		int i = 'A';
-		char c = 'b';
-		char c1 = '한';
-		try {
-			out.write(i);
-			out.write(c);
-			out.write(c1);
-			out.flush();	// 스트림에 남아있는 data를 비운다.
-			out.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
 
+	public static void main(String[] args) /*throws Exception*/{
+      OutputStream out = System.out;
+      int i = 'A';
+      char c = 'b';
+      char c1 = '한';
+      try {
+		out.write(i);
+		out.write(c);
+		out.write(c1);
+		out.flush(); //스트림에 남아있는 data를 비운다.
+		out.close();
+	} catch (IOException e) {
+		e.printStackTrace();
+	}  //print ,write 같은 것.
+      
+      
+   }
 }
